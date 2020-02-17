dd54c920009bd937385223c5729aead4770266f8
Talut TASGIRAN <hi@talut.dev>
2020-02-17 15:55:57 +0300

    v1.0.3 released

--- README.md
+++ README.md
@@ -10,7 +10,10 @@ Using [`ActionSheetIOS`](https://facebook.github.io/react-native/docs/actionshee
 ### Version >= 1.0.0 now has AndroidX support. If you want to use this package without AndroidX support please use v0.2.3
 #### Facebook RN blog post about v0.60 and AndroidX support: [https://facebook.github.io/react-native/blog/2019/07/03/version-60](https://facebook.github.io/react-native/blog/2019/07/03/version-60)
 
-### Version 1.0.2
+### Version 1.0.3
+- `tintColor` added for IOS
+
+#### Version 1.0.2
 - `implementation 'com.google.android.material:material:1.0.0'` issue solved
 
 ## Getting Started
@@ -48,6 +51,7 @@ react-native link rn-actionsheet-module
 |optionsAndroid         |array |**Yes**   |Android     |
 |title                  |string|**Yes**   |Android, IOS|
 |message                |string|No        |IOS         |    
+|tintColor              |string|No        |IOS         |    
 |destructiveButtonIndex |number|No        |IOS         |
 |cancelButtonIndex      |number|No        |IOS         |
 |onCancelAndroidIndex   |number|**Yes**   |Android     |
--- index.js
+++ index.js
@@ -4,11 +4,12 @@ import PropTypes from 'prop-types';
 const {RNActionsheet} = NativeModules;
 
 const ActionSheet = (props, callback) => {
-  const {optionsIOS, optionsAndroid, title, message, destructiveButtonIndex, cancelButtonIndex, onCancelAndroidIndex} = props;
+  const {optionsIOS, optionsAndroid, title, tintColor, message, destructiveButtonIndex, cancelButtonIndex, onCancelAndroidIndex} = props;
   if(Platform.OS === "ios") {
     ActionSheetIOS.showActionSheetWithOptions({
       title                 : title,
       message               : message,
+      tintColor             : tintColor,
       options               : optionsIOS,
       destructiveButtonIndex: destructiveButtonIndex === null ? undefined : destructiveButtonIndex,
       cancelButtonIndex     : cancelButtonIndex,
@@ -22,6 +23,7 @@ ActionSheet.propTypes = {
   optionsAndroid        : PropTypes.array.isRequired,
   title                 : PropTypes.string.isRequired,
   message               : PropTypes.string,
+  tintColor             : PropTypes.string,
   destructiveButtonIndex: PropTypes.number,
   cancelButtonIndex     : PropTypes.number,
   onCancelAndroidIndex  : PropTypes.number.isRequired,
